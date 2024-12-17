db3411eed891091ee759579fe645e52ca2067c54
anthonywong555 <anthonywong555@gmail.com>
2024-12-17 12:09:43 -0500

    Playing around shadcn 🔥

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import '../app.css';
+	import { Toaster } from "$lib/components/ui/sonner";
 </script>
 
+<Toaster />
 <slot></slot>
--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,5 +1,30 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
+  import { Input } from '$lib/components/ui/input';
+  import * as Card from "$lib/components/ui/card";
+  import * as Select from "$lib/components/ui/select";
+  import { Label } from "$lib/components/ui/label";
+
+  import { toast } from "svelte-sonner";
+ 
+  const frameworks = [
+    {
+      value: "sveltekit",
+      label: "SvelteKit"
+    },
+    {
+      value: "next",
+      label: "Next.js"
+    },
+    {
+      value: "astro",
+      label: "Astro"
+    },
+    {
+      value: "nuxt",
+      label: "Nuxt.js"
+    }
+  ];
   import type { PostWorkflowRequest, PostWorkflowResponse } from '@boilerplate/common';
 
   let name = '';
@@ -24,7 +49,55 @@
 </script>
 
 <h1>Web</h1>
-<input bind:value={name} placeholder="Enter your name" />
+<Card.Root class="w-[350px]">
+  <Card.Header>
+    <Card.Title>Create project</Card.Title>
+    <Card.Description>Deploy your new project in one-click.</Card.Description>
+  </Card.Header>
+  <Card.Content>
+    <form>
+      <div class="grid w-full items-center gap-4">
+        <div class="flex flex-col space-y-1.5">
+          <Label for="name">Name</Label>
+          <Input id="name" placeholder="Name of your project" />
+        </div>
+        <div class="flex flex-col space-y-1.5">
+          <Label for="framework">Framework</Label>
+          <Select.Root>
+            <Select.Trigger id="framework">
+              <Select.Value placeholder="Select" />
+            </Select.Trigger>
+            <Select.Content>
+              {#each frameworks as framework}
+                <Select.Item value={framework.value} label={framework.label}
+                  >{framework.label}</Select.Item
+                >
+              {/each}
+            </Select.Content>
+          </Select.Root>
+        </div>
+      </div>
+    </form>
+  </Card.Content>
+  <Card.Footer class="flex justify-between">
+    <Button variant="outline">Cancel</Button>
+    <Button>Deploy</Button>
+  </Card.Footer>
+</Card.Root>
+<Input type="text" placeholder="First Name" class="" bind:value={name} />
 <Button on:click={handleFormSubmit}>Submit</Button>
+<Button
+  variant="outline"
+  on:click={() =>
+    toast.success("Event has been created", {
+      description: "Sunday, December 03, 2023 at 9:00 AM",
+      action: {
+        label: "Undo",
+        onClick: () => console.info("Undo")
+      }
+    })}
+>
+  Show Toast
+</Button>
 <p>{greet}</p>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
